22950ac595c6c52376272a1216f2a5cbc7ad953b
hburnton <hburnton@gmail.com>
2022-10-24 17:23:11 -0700

    add view all roles func

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (name)
+VALUES
+    ("Customer Service"),
+    ("Finance"),
+    ("Public Relations"),
+    ("Technology");
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Big Rush Man", 80000, 3),
+    ("Small Rush Man", 60000, 4),
+    ("Super Big Man", 100000, 1),
+    ("Guy who does nothing", 15000, 2);
+
+INSERT INTO employee(first_name, last_name)
+VALUES
+    ("Steve", "Pinker"),
+    ("Florence", "Butker"),
+    ("Mikey", "Flores"),
+    ("Jane", "Campion"),
+    ("Wallace", "Wilton");  
