89eb1cd233d3b1560ad242c2eab2b997f7c4812e
hburnton <hburnton@gmail.com>
2022-10-27 20:41:39 -0700

    attempt add employee promise

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const mysql = require('./config/connection');
 require('console.table');
 
+//Question Department for Inquirer Static Questions
 const addDepartmentQuestions = [
     {
         type: 'input',
@@ -99,6 +100,57 @@ const viewEmployees = () =>{
 }
 
 //ADD EMPLOYEES - COMPLETED
+//REWRITTEN ATTEMPTING PROMISE QUERY (I'm not really sure where to put catches/this is still really nested trying to work it out)
+const addEmployee = () =>{
+
+    inquirer.prompt(addEmployeeQuestions)
+    .then(({firstName, lastName})=>{
+
+        mysql.promise().query(`SELECT role.id, role.title FROM role`)
+        .then( (results) =>{
+            const employeeRoles = results[0].map(({ id, title }) => ({ name: title, value: id }));
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'newEmployeeRole',
+                    message: 'Please select a role for this employee',
+                    choices: employeeRoles
+                }
+            ])
+            .then((result)=>{
+                const employeeRoleId = result.newEmployeeRole;
+
+                mysql.promise().query('SELECT * FROM employee')
+                .then(results=>{
+                    const managerList = results[0].map(({id, first_name, last_name})=>({name: `${first_name} ${last_name}`, value: id}));
+                    inquirer.prompt([
+                        {
+                            type:'list',
+                            name: 'newEmployeeManager',
+                            message: 'Please select the new employee\'s manager. \n',
+                            choices: managerList
+                        }
+                    ])
+                    .then(result=>{
+                        const managerId = result.newEmployeeManager;
+
+                        mysql.promise().query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+                        VALUES ('${firstName}', '${lastName}', ${employeeRoleId}, ${managerId})`)
+                        .then(result =>{
+                            console.log(`New Employee: ${firstName} ${lastName} has been added!`);
+                            printMenu();
+                        });
+                    });
+                });
+            });
+        });
+    })
+    .catch(err =>{
+        console.log(err)
+    });
+};
+
+/*
 const addEmployee = () =>{
 
     inquirer.prompt(addEmployeeQuestions)
@@ -154,6 +206,9 @@ const addEmployee = () =>{
         });
     });
 };
+*/
+
+
 
 //addRole - COMPLETED
 const addRole= () =>{
@@ -236,6 +291,7 @@ const updateEmployeeRole = () =>{
         });
     });
 };
+
 
 const menuRouter = (response) =>{
     switch(response.menuChoice){
